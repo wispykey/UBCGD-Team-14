559a1a7376d70da3dc2187a93f775d5078bfbf5e
mwang516 <mwang516@students.cs.ubc.ca>
2024-11-04 14:45:55 -0800

    test

--- Player/player.gd
+++ Player/player.gd
@@ -6,7 +6,6 @@ var tileMap = get_node("../Background")
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- Prototyping/Example/danger_sign.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Timer.timeout.connect(_on_timer_timeout)
-
-func _on_timer_timeout():
-	# Removes this node from the scene tree
-	queue_free()
