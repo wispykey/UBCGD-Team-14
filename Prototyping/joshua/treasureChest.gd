e43e07cb5d977fa81ec01871ab223176de9c7055
joshuac23 <josh_jay23@hotmail.com>
2024-11-12 15:32:39 -0800

    Added Animated Chest to Level
    Made a script for animated chest. Chest opens and plays animation only if player has a key

--- Prototyping/joshua/treasureChest.gd
+++ Prototyping/joshua/treasureChest.gd
@@ -1,3 +1,13 @@
-@tool
+extends Area2D
 
-class_name TreasureChest extends Node2D
+@onready var animation: AnimationPlayer = $"../AnimationPlayer"
+
+
+var isOpened = false
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player" and not isOpened and body.hasKey:  # Check if the touching object is the player
+		print("CHEST OPENED")
+		animation.play("chestContentsRevealed")
+		isOpened = true
+		body.hasKey = false		
+		hide()
