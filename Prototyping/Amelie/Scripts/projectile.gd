265501f12b59aa22ac4dddb9bfe8d585ab0f3f55
DoubtRabbit <amelka3911@gmail.com>
2024-11-11 22:32:33 -0700

    week 2

---
+++ Prototyping/Amelie/Scripts/projectile.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+const SPEED = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y = position.y + SPEED
