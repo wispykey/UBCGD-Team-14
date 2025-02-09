b1821e6f07585ed6f494f4e0bad6e574de35d822
MCNewbie101 <94023224+MCNewbie101@users.noreply.github.com>
2025-02-08 17:33:23 -0800

    Added iterative projectile variations
    Still need hitboxes

--- EnemyAttacks/Projectiles/iterative_projectile.gd
+++ EnemyAttacks/Projectiles/iterative_projectile.gd
@@ -1,44 +1,52 @@
 extends Node2D
 
 #TODO: Add terrian collision so the projectile disappers when it hit a wall
-#      (Maybe make a variable that would make it pass through the wall instead at some point?)
 #TODO: Implement other types of projectiles
-#TODO: Erase sprites after the projectile moves
+
+@onready var player_coords
 
 @export var telegraph_image_west: PackedScene
 @export var telegraph_image_east: PackedScene
 @export var telegraph_image_north: PackedScene
 @export var telegraph_image_south: PackedScene
+@export var telegraph_image_expanding: PackedScene
+@export var telegraph_image_spinning_west: PackedScene
+@export var telegraph_image_spinning_east: PackedScene
+@export var telegraph_image_spinning_north: PackedScene
+@export var telegraph_image_spinning_south: PackedScene
+@export var telegraph_image_tracking: PackedScene
 @export var generate_projectile: PackedScene
 
 const DIRECTIONS = ["WEST", "EAST", "NORTH", "SOUTH"]
 const TYPE = ["STRAIGHT", "EXPANDING", "SPINNING", "TRACKING"]
 const TILE_SIZE = 32
 
-var telegraph_duration: int  = 4 # Measured in beats
+var telegraph_duration: int = 4 # Measured in beats
 var direction: String = ""
 var type: String = ""
 var projectiles = []  # List of attacks
+var starting_coords = []  # List of starting coordinates, measured in tiles
 var speed = 1
 var damage: int = 5
 var count: int = 0
+var turn_count: int # Spinning projectiles only
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$TelegraphTimer.wait_time = telegraph_duration * Conductor.seconds_per_quarter_note
 	$TelegraphTimer.timeout.connect(_on_telegraph_timer_timeout)
-	$UpdateTimer.wait_time = telegraph_duration * Conductor.seconds_per_quarter_note
+	$UpdateTimer.wait_time = Conductor.seconds_per_quarter_note
 	$UpdateTimer.timeout.connect(_on_update_timer_timeout)
 
-func start(input_direction: String, input_type: String, input_coords: PackedVector2Array):
+func start(input_direction: String, input_type: String, input_coords: PackedVector2Array, input_turn_count: int):
 	print("Projectile generated")
 	set_direction(input_direction)
 	set_attack_type(input_type)
-	match type:
-		"STRAIGHT":
-			for i in range(input_coords.size()):
-				projectiles.append(generate_projectile.instantiate())
-				projectiles[i].start(direction, input_coords[i])
+	for i in range(input_coords.size()):
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[i].start(direction, input_coords[i])
+		starting_coords.append(input_coords[i])
+	turn_count = input_turn_count
 	$TelegraphTimer.start()
 	match type:
 		"STRAIGHT":
@@ -51,15 +59,29 @@ func start(input_direction: String, input_type: String, input_coords: PackedVect
 					add_scene($Telegraph, telegraph_image_north);
 				"SOUTH":
 					add_scene($Telegraph, telegraph_image_south);
+		"EXPANDING":
+				add_scene($Telegraph, telegraph_image_expanding);
+		"SPINNING":
+			match direction:
+				"WEST":
+					add_scene($Telegraph, telegraph_image_spinning_west);
+				"EAST":
+					add_scene($Telegraph, telegraph_image_spinning_east);
+				"NORTH":
+					add_scene($Telegraph, telegraph_image_spinning_north);
+				"SOUTH":
+					add_scene($Telegraph, telegraph_image_spinning_south);
+		"TRACKING":
+			add_scene($Telegraph, telegraph_image_tracking);
 
-func start_one_coord(input_direction: String, input_type: String, input_coord: Vector2):
+func start_one_coord(input_direction: String, input_type: String, input_coord: Vector2, input_turn_count: int):
 	print("Projectile generated")
 	set_direction(input_direction)
 	set_attack_type(input_type)
-	match type:
-		"STRAIGHT":
-			projectiles.append(generate_projectile.instantiate())
-			projectiles[0].start(direction, input_coord)
+	projectiles.append(generate_projectile.instantiate())
+	projectiles[0].start(direction, input_coord)
+	starting_coords.append(input_coord)
+	turn_count = input_turn_count
 	$TelegraphTimer.start()
 	match type:
 		"STRAIGHT":
@@ -72,7 +94,20 @@ func start_one_coord(input_direction: String, input_type: String, input_coord: V
 					add_scene($Telegraph, telegraph_image_north);
 				"SOUTH":
 					add_scene($Telegraph, telegraph_image_south);
-				
+		"EXPANDING":
+				add_scene($Telegraph, telegraph_image_expanding);
+		"SPINNING":
+			match direction:
+				"WEST":
+					add_scene($Telegraph, telegraph_image_spinning_west);
+				"EAST":
+					add_scene($Telegraph, telegraph_image_spinning_east);
+				"NORTH":
+					add_scene($Telegraph, telegraph_image_spinning_north);
+				"SOUTH":
+					add_scene($Telegraph, telegraph_image_spinning_south);
+		"TRACKING":
+			add_scene($Telegraph, telegraph_image_tracking);
 
 # Called every beat.
 #TODO: Make this based on beat, not time
@@ -80,12 +115,106 @@ func update() -> void:
 	for child in get_children():
 		if child is not Timer:
 			remove_child(child)
-	if type == "STRAIGHT":
-		for projectile in projectiles:
-			projectile.move()
-			projectile.add_scene(self)
-#	add_scene(self)
+	match type:
+		"STRAIGHT":
+			for projectile in projectiles:
+				projectile.move()
+				projectile.add_scene(self)
+		"EXPANDING":
+			add_corners()
+			count += 1
+			for projectile in projectiles:
+				projectile.move()
+				projectile.add_scene(self)
+		"SPINNING":
+			if count >= turn_count - 1:
+				match projectiles[0].direction:
+					"WEST":
+						for projectile in projectiles:
+							projectile.direction = "NORTH"
+					"SOUTH":
+						for projectile in projectiles:
+							projectile.direction = "WEST"
+					"EAST":
+						for projectile in projectiles:
+							projectile.direction = "SOUTH"
+					"NORTH":
+						for projectile in projectiles:
+							projectile.direction = "EAST"
+				count = 0
+			for projectile in projectiles:
+				projectile.move()
+				projectile.add_scene(self)
+			count += 1
+		"TRACKING":
+			player_coords = get_node("../Player").position
+			print(floor(player_coords.x / TILE_SIZE))
+			print(floor(player_coords.y / TILE_SIZE))
+			for projectile in projectiles:
+				if floor(player_coords.y / TILE_SIZE) < projectile.coord.y:
+					if floor(player_coords.x / TILE_SIZE) < projectile.coord.x:
+						if projectile.coord.x - floor(player_coords.x / TILE_SIZE) < projectile.coord.y - floor(player_coords.y / TILE_SIZE):
+							projectile.direction = "NORTH"
+						else:
+							projectile.direction = "WEST"
+					else:
+						if projectile.coord.x - floor(player_coords.x / TILE_SIZE) < player_coords.y - floor(player_coords.y / TILE_SIZE):
+							projectile.direction = "NORTH"
+						else:
+							projectile.direction = "EAST"
+				else:
+					if floor(player_coords.x / TILE_SIZE) < projectile.coord.x:
+						if floor(player_coords.x / TILE_SIZE) - projectile.coord.x < projectile.coord.y - floor(player_coords.y / TILE_SIZE):
+							projectile.direction = "SOUTH"
+						else:
+							projectile.direction = "WEST"
+					else:
+						if floor(player_coords.x / TILE_SIZE) - projectile.coord.x < floor(player_coords.y / TILE_SIZE) - projectile.coord.y:
+							projectile.direction = "SOUTH"
+						else:
+							projectile.direction = "EAST"
+				projectile.move()
+				projectile.add_scene(self)
 	$UpdateTimer.start()
+
+# Adds courners to expanding projectiles
+func add_corners():
+	for coord in starting_coords:
+		var to_generate: Vector2
+		to_generate.x = coord.x - count
+		to_generate.y = coord.y - count
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("NORTH", to_generate)
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("WEST", to_generate)
+		to_generate.x -= 1
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("NORTH", to_generate)
+		to_generate.x = coord.x + count
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("EAST", to_generate)
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("NORTH", to_generate)
+		to_generate.y -= 1
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("EAST", to_generate)
+		to_generate.y = coord.y + count
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("SOUTH", to_generate)
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("EAST", to_generate)
+		to_generate.x += 1
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("SOUTH", to_generate)
+		to_generate.x = coord.x - count
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("WEST", to_generate)
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("SOUTH", to_generate)
+		to_generate.y += 1
+		projectiles.append(generate_projectile.instantiate())
+		projectiles[-1].start("WEST", to_generate)
+
 
 # Allows parent nodes to set orientation of attack
 func set_direction(cardinal: String):
--- Prototyping/Example/iterative_projectile_testing.gd
+++ Prototyping/Example/iterative_projectile_testing.gd
@@ -13,25 +13,21 @@ const TILE_SIZE: int = 32
 const HOR_TILES: int = WIDTH / TILE_SIZE
 const VER_TILES: int = HEIGHT / TILE_SIZE
 
-#const TILES = {
-	#"RED": {"src": 2, "atlas": Vector2(0,0), "alt": 3},
-	#"DANGER": {"src": 3, "atlas": Vector2(0, 0), "alt": 0},
-	#"FLOOR_DARK": {"src": 1, "atlas": Vector2(0,0), "alt": 3},
-	#"FLOOR_MED": {"src": 1, "atlas": Vector2(0,0), "alt": 1},
-	#"FLOOR_LIGHT": {"src": 1, "atlas": Vector2(0,0), "alt": 0},
-	#"GHOST": {"src": 0, "atlas": Vector2(0, 0), "alt": 0},
-#}
-#const DEFAULT_FLOOR = TILES.FLOOR_DARK
-
 # A queue of scheduled function calls.
 # 	'time' is measured in beats; must be strictly non-decreasing
 #   'function' is the name of a function in this script; must be in quotation marks
 # 	'args' is a dictionary of additional parameters to 'function'
 var timeline = [
-	{"time": 4, "function": "spawn_projectile_at_random", "args": {}},
-	{"time": 8, "function": "spawn_projectile_at_random", "args": {"direction": "NORTH"}},
-	{"time": 12, "function": "spawn_projectile_at_random", "args": {"direction": "SOUTH"}},
-	{"time": 16, "function": "spawn_projectile_at_random", "args": {"direction": "WEST"}},
+	{"time": 12, "function": "spawn_projectile_at_position", "args": {"coord_x": 5, "coord_y": 6}},
+	{"time": 16, "function": "spawn_projectile_at_position", "args": {"direction": "NORTH", "coord_x": 3, "coord_y": 5}},
+	{"time": 20, "function": "spawn_projectile_at_position", "args": {"direction": "SOUTH", "coord1_x": 15, "coord1_y": 5, "coord2_x": 16, "coord2_y": 5, "coord3_x": 17, "coord3_y": 5}},
+	{"time": 24, "function": "spawn_projectile_at_position", "args": {"direction": "WEST", "coord1_x": 7, "coord1_y": 5, "coord2_x": 9, "coord2_y": 9}},
+	{"time": 40, "function": "spawn_projectile_at_position", "args": {"direction": "WEST", "type": "EXPANDING", "coord_x": 5, "coord_y": 5}},
+	{"time": 60, "function": "spawn_projectile_at_position", "args": {"direction": "WEST", "type": "SPINNING", "turn": "4", "coord_x": 5, "coord_y": 5}},
+	{"time": 64, "function": "spawn_projectile_at_position", "args": {"direction": "NORTH", "type": "SPINNING", "turn": "3", "coord_x": 5, "coord_y": 7}},
+	{"time": 68, "function": "spawn_projectile_at_position", "args": {"direction": "SOUTH", "type": "SPINNING", "turn": "7", "coord_x": 15, "coord_y": 3}},
+	{"time": 72, "function": "spawn_projectile_at_position", "args": {"direction": "WEST", "type": "SPINNING", "coord_x": 7, "coord_y": 4}},
+	{"time": 78, "function": "spawn_projectile_at_position", "args": {"direction": "WEST", "type": "TRACKING", "coord_x": 10, "coord_y": 10}},
 ]
 var next_event: int = 0
 
@@ -76,21 +72,33 @@ func get_random_position() -> Vector2:
 	return Vector2(random_x, random_y)
 	
 
+# Can spawn up to 5 projectiles at athe same time currently
 func spawn_projectile_at_position(args: Dictionary):
 	var projectile = spawn_projectile.instantiate()
 	var type  = args.type if args.has("type") else ""
 	var direction  = args.direction if args.has("direction") else ""
+	var turn_count = int(args.turn) if args.has("turn") else 0
 	var coords
-	if (args.has("coords")):
-		coords = args.coords
-		projectile.start(direction, type, coords)
+	if (args.has("coord1_x") && args.has("coord1_y")):
+		coords = PackedVector2Array()
+		coords.append(Vector2(int(args.coord1_x), int(args.coord1_y)))
+		if (args.has("coord2_x") && args.has("coord2_y")):
+			coords.append(Vector2(int(args.coord2_x), int(args.coord2_y)))
+			if (args.has("coord3_x") && args.has("coord3_y")):
+				coords.append(Vector2(int(args.coord3_x), int(args.coord3_y)))
+				if (args.has("coord4_x") && args.has("coord4_y")):
+					coords.append(Vector2(int(args.coord4_x), int(args.coord4_y)))
+					if (args.has("coord5_x") && args.has("coord5_y")):
+						coords.append(Vector2(int(args.coord5_x), int(args.coord5_y)))
+		add_child(projectile)
+		projectile.start(direction, type, coords, turn_count)
 		return
-	elif (args.has("coord")):
-		coords = args.coord
+	elif (args.has("coord_x") && args.has("coord_y")):
+		coords = Vector2(int(args.coord_x), int(args.coord_y))
 	else:
 		coords = Vector2(0, 0)
 	add_child(projectile)
-	projectile.start_one_coord(direction, type, coords)
+	projectile.start_one_coord(direction, type, coords, turn_count)
 	
 
 func spawn_projectile_at_random(args: Dictionary):
@@ -98,5 +106,6 @@ func spawn_projectile_at_random(args: Dictionary):
 	var type  = args.type if args.has("type") else ""
 	var direction  = args.direction if args.has("direction") else ""
 	var coords = get_random_position()
+	var turn_count = int(args.turn) if args.has("turn") else 2
 	add_child(projectile)
-	projectile.start_one_coord(direction, type, coords)
+	projectile.start_one_coord(direction, type, coords, turn_count)
