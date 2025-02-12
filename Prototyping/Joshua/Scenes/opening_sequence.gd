e3792f02e73aa9dc7f255e77a4686c4f980e2eb3
joshuac23 <josh_jay23@hotmail.com>
2025-02-11 22:47:38 -0800

    Opening sequence tweaks
    -added blinking continue text on title screen

--- Prototyping/Joshua/Scenes/opening_sequence.gd
+++ Prototyping/Joshua/Scenes/opening_sequence.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-
+@onready var continue_label = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var animation = $AnimationPlayer
+	continue_label.hide()
 	animation.play("title_sequence")
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,3 +16,9 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		get_tree().change_scene_to_file("res://Prototyping/Joshua/Scenes/main_menu.tscn")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	continue_label.show()
+	var label_animation = $ContinueText
+	label_animation.play("continue_text")
