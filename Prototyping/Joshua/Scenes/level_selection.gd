35ce1b70577d6ceddd7b1ed346e35fc65db23646
joshuac23 <josh_jay23@hotmail.com>
2025-02-14 02:18:05 -0800

    Added level image previews
    -put image previews in the level description box for each level

--- Prototyping/Joshua/Scenes/level_selection.gd
+++ Prototyping/Joshua/Scenes/level_selection.gd
@@ -5,15 +5,18 @@ var current_index = 0
 var level_map = {
 	"Spookesville": {	"scene": "res://Levels/Supernatural.tscn",
 						"image": preload("res://Prototyping/Joshua/Assets/supernaturalPortal.png"),
-						"info": "A mist-shrouded realm where flickering lanterns cast eerie glows over crumbling mausoleums. Once a haven for harmonious ghostly existence, the land is now gripped by turmoil as a vengeful spirit rises, threatening to shatter the delicate peace"
+						"info": "A mist-shrouded realm where flickering lanterns cast eerie glows over crumbling mausoleums. Once a haven for harmonious ghostly existence, the land is now gripped by turmoil as a vengeful spirit rises, threatening to shatter the delicate peace",
+						"preview": preload("res://Prototyping/Joshua/Assets/supernatural_preview1.png")
 						},
 	"Spellville": { 	"scene": "res://Levels/Fantasy.tscn",
 						"image": preload("res://Prototyping/Joshua/Assets/fantasyPortal.png"),
-						"info": "A vibrant fantasy town where magic weaves through everyday life, from glowing cobblestone streets to floating marketplaces. Unbeknownst to its residents, a rogue magic now threatens to unravel its harmony."
+						"info": "A vibrant fantasy town where magic weaves through everyday life, from glowing cobblestone streets to floating marketplaces. Unbeknownst to its residents, a rogue magic now threatens to unravel its harmony.",
+						"preview": preload("res://Prototyping/Joshua/Assets/fantasy_preview1.png")
 						},
 	"Steamville": {		"scene": "",
 						"image": preload("res://Prototyping/Joshua/Assets/steampunkPortal.png"),
-						"info": "A steampunk metropolis of towering brass spires, airships, and intricate machinery, where innovation thrives amidst the hum of gears. Beneath its gleaming surface, a shadowy faction seeks to sabotage the city's power core, threatening to plunge it into darkness."
+						"info": "A steampunk metropolis of towering brass spires, airships, and intricate machinery, where innovation thrives amidst the hum of gears. Beneath its gleaming surface, a shadowy faction seeks to sabotage the city's power core, threatening to plunge it into darkness.",
+						"preview": preload("res://Prototyping/Joshua/Assets/fantasy_preview1.png") # change me
 						}
 }
 
@@ -79,3 +82,6 @@ func getCurrentLevel():
 	
 func getLevelInfo():
 	return level_map[levels[current_index]].info
+	
+func getCurrentLevelPreview():
+	return level_map[levels[current_index]].preview
