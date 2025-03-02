124e1a956741f0fac0d967034caa513bf9f6b0dd
wispykey <hui.keelan@gmail.com>
2025-03-02 10:55:16 -0800

    Basic growth and recede

---
+++ Enemies/EnemyAttacks/VineCross/vine_cross.gd
@@ -0,0 +1,70 @@
+extends Node2D
+
+
+var growth_rate: int = 150
+var all_directions_finished: bool = false
+
+# The longest distance any vine has to travel before hitting wall
+var longest_distance: int
+
+var vines = []
+# Order is coupled with vines
+var directions = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	vines = [$Right, $Left, $Up, $Down]
+	compute_furthest_direction()
+	
+	print(Conductor.seconds_per_quarter_note)
+	$TelegraphTimer.wait_time = 4 * Conductor.seconds_per_quarter_note
+	$LifetimeTimer.wait_time = 10 * Conductor.seconds_per_quarter_note
+	
+
+func compute_furthest_direction():
+	var spawn_position = global_position
+	
+	# Since we have one layer of 'wall' in arena, we can just use arena size
+	var to_left_wall = spawn_position.x
+	var to_right_wall = GameState.control_port.end.x - spawn_position.x
+	var to_upper_wall = spawn_position.y
+	var to_lower_wall = GameState.control_port.end.y - spawn_position.y
+
+	longest_distance = max(to_left_wall, to_right_wall, to_upper_wall, to_lower_wall)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !$TelegraphTimer.is_stopped():
+		return 
+	if !all_directions_finished:
+		grow(delta)
+	elif all_directions_finished and $LifetimeTimer.is_stopped():
+		recede(delta)
+
+
+func grow(delta: float):
+	for i in range(len(vines)):
+		var vine = vines[i]
+		var last_point = vine.points[vine.get_point_count()-1]
+		var new_point = last_point + delta*directions[i]*growth_rate
+		vine.add_point(new_point)
+		check_bounds(new_point)
+	update_collision_regions()
+	
+func update_collision_regions():
+	pass
+	
+func recede(delta: float):
+	for i in range(len(vines)):
+		var vine = vines[i]
+		if (vine.points.size() > 2):
+			var point_to_remove = vine.points[vine.points.size()-1]
+			print(point_to_remove)
+			vine.remove_point(vine.points.size()-1)
+
+func check_bounds(point):
+	if point.x > longest_distance or point.y > longest_distance:
+		all_directions_finished = true
+		print("Finished")
+		$LifetimeTimer.start()
