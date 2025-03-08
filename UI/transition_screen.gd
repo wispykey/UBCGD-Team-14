240e48a05f917f17d7301d160083d537118264b5
joshuac23 <josh_jay23@hotmail.com>
2025-03-08 03:55:20 -0800

    Added "loading..." flame animation to transitions

--- UI/transition_screen.gd
+++ UI/transition_screen.gd
@@ -5,9 +5,11 @@ signal on_transition_finished
 
 @onready var color_rect = $ColorRect
 @onready var animation_player = $AnimationPlayer
+@onready var flame_loader = $CPUParticles2D
 
 func _ready():
 	color_rect.visible = false
+	flame_loader.visible = false
 	animation_player.animation_finished.connect(_on_animation_finished)
 	
 func _on_animation_finished(anim_name):
@@ -16,9 +18,11 @@ func _on_animation_finished(anim_name):
 		animation_player.play("fade_to_normal")
 	elif anim_name == "fade_to_normal":
 		color_rect.visible = false
+		flame_loader.visible = false
 	
 func transition():
 	color_rect.visible = true
+	flame_loader.visible = true
 	animation_player.play("fade_to_black")
 
 #signal transitioned
