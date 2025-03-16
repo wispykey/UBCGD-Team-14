94cc4bc1eb474ab35ed181b53e4ed04d7e32f3f3
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2025-03-16 10:58:00 -0700

    removing print statements

--- UI/GameMenu.gd
+++ UI/GameMenu.gd
@@ -11,11 +11,8 @@ func _on_home_button_pressed() -> void:
 func _on_restart_button_pressed() -> void:
 	var path: String = get_parent().get_path();
 	if path.ends_with("Fantasy"):
-		print("Fantasy")
 		get_tree().change_scene_to_file("res://Levels/Fantasy.tscn")
 	elif path.ends_with("Supernatural"):
-		print("Supernatural")
 		get_tree().change_scene_to_file("res://Levels/Supernatural.tscn")
 	else:
-		print("Error home")
 		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
