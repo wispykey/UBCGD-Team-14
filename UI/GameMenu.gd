514ce6a53f5fd13149eb80082fadf62e71cc4957
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2025-03-16 10:55:49 -0700

    adding restart and skip dialog

--- UI/GameMenu.gd
+++ UI/GameMenu.gd
@@ -9,5 +9,13 @@ func _on_home_button_pressed() -> void:
 
 
 func _on_restart_button_pressed() -> void:
-	# TODO reset the game
-	pass # Replace with function body.
+	var path: String = get_parent().get_path();
+	if path.ends_with("Fantasy"):
+		print("Fantasy")
+		get_tree().change_scene_to_file("res://Levels/Fantasy.tscn")
+	elif path.ends_with("Supernatural"):
+		print("Supernatural")
+		get_tree().change_scene_to_file("res://Levels/Supernatural.tscn")
+	else:
+		print("Error home")
+		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
