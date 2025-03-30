56a1ad8232961c19dcd54796f91ad4b7827f24a2
wispykey <hui.keelan@gmail.com>
2025-03-29 18:12:48 -0700

    Reparent boss and staff to fix scale

--- Enemies/EnemyAttacks/boss_staff.gd
+++ Enemies/EnemyAttacks/boss_staff.gd
@@ -4,10 +4,11 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	offset = Vector2(40, -12)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	speed_scale = 1 / Conductor.seconds_per_quarter_note 
 	anim.speed_scale = 1 / Conductor.seconds_per_quarter_note
+	global_position = %FantasyBoss.global_position
