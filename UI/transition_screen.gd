1fd508906ca8bfd8683b51e67ec6021983814ac7
wispykey <hui.keelan@gmail.com>
2025-03-29 20:20:22 -0700

    Uncomment necessary signal connection

--- UI/transition_screen.gd
+++ UI/transition_screen.gd
@@ -10,7 +10,7 @@ signal on_transition_finished
 func _ready():
 	color_rect.visible = false
 	flame_loader.visible = false
-	#animation_player.animation_finished.connect(_on_animation_finished)
+	animation_player.animation_finished.connect(_on_animation_finished)
 	
 func _on_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
